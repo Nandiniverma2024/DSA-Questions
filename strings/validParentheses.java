8b4be77090b0116f45c0a888619deafb409c9a41
Nandini <nandiniverma202021@gmail.com>
2025-07-02 21:32:48 +0530

    Add String folder for java string questions

---
+++ strings/validParentheses.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+// BruteForce Approach(Stack based approach)
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                if(!stack.isEmpty()){
+                    result.append(ch);
+                }
+                stack.push(ch);
+            }else if(ch == ')'){
+                stack.pop();
+
+                // If stack is not empty after pop, it's not the outermost closing bracket,add it to result
+                if(!stack.isEmpty()){
+                    result.append(ch);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
+
+// Optimal approach(Counter based Approach)
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ')'){
+                count--; 
+            }
+           // count will only be 0, when we get outermost closing bracket, 
+           // that's why 1st condition is for closing bracket
+
+            if(count != 0){
+                result.append(s.charAt(i));
+            }
+            if(s.charAt(i) == '('){
+                count++;
+            }
+        }
+        return result.toString();
+    }
+}
