2663f8be7f165f7a529d3a8e4cad0cef988eb7db
Nandini <nandiniverma202021@gmail.com>
2025-07-05 11:38:23 +0530

    create folder for potd

---
+++ POTD/ARRAY/luckynumber.java
@@ -0,0 +1,20 @@
+// 1394. Find Lucky Integer in an Array
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+ 
+
+// Example 1:
+
+// Input: arr = [2,2,3,4]
+// Output: 2
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+
+
