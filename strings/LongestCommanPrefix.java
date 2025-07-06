33050b4bcd99e167e779258d6247a6f3f141cb11
Nandini <nandiniverma202021@gmail.com>
2025-07-06 23:12:52 +0530

    add: Optimal Approach for longest comman prefix problem

--- strings/LongestCommanPrefix.java
+++ strings/LongestCommanPrefix.java
@@ -55,3 +55,31 @@ class Solution {
 
 
 // tc adn sc -> O(N*M)	O(1)
+
+
+
+
+
+
+// Optimal Approach
+import java.util.Arrays;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        int i = 0;
+        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+
+        return first.substring(0, i);
+    }
+}
+
+// tc and sc -> O(N log N + M)	O(1)
+
