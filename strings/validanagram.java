85f5383366962ea1c9fc3a169cf616036347c464
Nandini <nandiniverma202021@gmail.com>
2025-07-14 23:42:09 +0530

    add: optimal approach

--- strings/validanagram.java
+++ strings/validanagram.java
@@ -35,3 +35,46 @@ class Solution {
 // sc -> O(n) (since created two new arrays)
 
 
+
+// Optimal Approach
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // If string lengths are not equal then they never be an anagram
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+
+        // This loop tracks character count of s and t
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        // If all values are zeros , then it's an anagram
+        for(int i=0; i<26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+// s.charAt(i) - 'a' -> meaning
+// Character	ASCII	'char' - 'a'	Index in count[]
+//    'a'	     97	     97 - 97 = 0	     0
+//    'b'	     98	     98 - 97 = 1	     1
+//    'z'	     122     122 - 97 = 25	     25
+
+
+
+// Time and space complexity
+// tc -> O(n + 26) = O(n)
+// sc -> O(1) (since, only create a fixed size array count)
+
+
+
+
