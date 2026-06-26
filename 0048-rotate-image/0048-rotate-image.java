2b45b836ca9d849c7ed71914e1c0525e4c8269f6
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-06-26 10:55:16 +0530

    Time: 0 ms (100%), Space: 43.5 MB (83.23%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,24 +1,28 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        // Transpose Matrix
+        int m=matrix[0].length;
+        // Transpose
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                swap(matrix, i, j, j, i);
+            for(int j=i+1; j<m; j++){ //j=>i+1, to preserve diagonal element
+                int temp=matrix[j][i];
+                matrix[j][i]=matrix[i][j];
+                matrix[i][j]=temp;
             }
         }
-        // Reverse Each Row
+
+        // Reverse each row
         for(int i=0; i<n; i++){
-            for(int j=0; j<n/2; j++){ //if n=3, j<1 , i.e j runs till j=0
-                swap(matrix, i, j, i, n-j-1); //n-j-1 => gives last index of each row
+            int left=0;
+            int right=matrix[i].length-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+
+                left++;
+                right--;
             }
         }
-    }
-    public void swap(int matrix[][], int i1, int j1, int i2, int j2){
-        int temp=matrix[i1][j1];
-        matrix[i1][j1]=matrix[i2][j2];
-        matrix[i2][j2]=temp;
     }
 }
-
-// Rotation = Transpose + Horizontal Flip(rotate each row)
