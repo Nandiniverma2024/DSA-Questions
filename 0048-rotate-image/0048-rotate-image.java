8233b38ff4e570c68416fd231f371fc8bc7f469c
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-06-26 10:34:28 +0530

    Time: 0 ms (100%), Space: 43.7 MB (51.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,38 +1,24 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length; //row
-        int m=matrix[0].length; //col
-
-
-        int transpose[][]=new int[n][m];
-        int o=transpose.length;
-        int p=transpose[0].length;
-
-        // Transpose
+        int n=matrix.length;
+        // Transpose Matrix
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                transpose[j][i]=matrix[i][j];
+            for(int j=i+1; j<n; j++){
+                swap(matrix, i, j, j, i);
             }
         }
-
-        // Reverse each row
+        // Reverse Each Row
         for(int i=0; i<n; i++){
-            int left=0;
-            // matrix[i].length => gives length of each row
-            int right=transpose[i].length-1;
-            while(left<right){
-                int temp=transpose[i][left];
-                transpose[i][left]=transpose[i][right];
-                transpose[i][right]=temp;
-                left++;
-                right--;
-            }
-        }
-        for(int i=0; i<o; i++){
-            for(int j=0; j<p; j++){
-                matrix[i][j]=transpose[i][j];
+            for(int j=0; j<n/2; j++){ //if n=3, j<1 , i.e j runs till j=0
+                swap(matrix, i, j, i, n-j-1); //n-j-1 => gives last index of each row
             }
         }
     }
-    
+    public void swap(int matrix[][], int i1, int j1, int i2, int j2){
+        int temp=matrix[i1][j1];
+        matrix[i1][j1]=matrix[i2][j2];
+        matrix[i2][j2]=temp;
+    }
 }
+
+// Rotation = Transpose + Horizontal Flip(rotate each row)
