1d92df21289ade3d009a4e65a73fd8d7e8a2c72d
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-07-10 11:42:21 +0530

    Time: 3 ms (7.98%), Space: 43.6 MB (6.4%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+
         if(goal.length()!=s.length()){
             return false;
         }
-        String dblS=s+s;
-        if(dblS.contains(goal)){
-            return true;
+
+        String curr=s;
+        for(int i=0; i<s.length(); i++){
+            if(curr.equals(goal)){
+                return true;
+            }
+            //check rotations
+            curr=curr.substring(1)+s.charAt(i);
         }
+
         return false;
     }
 }
