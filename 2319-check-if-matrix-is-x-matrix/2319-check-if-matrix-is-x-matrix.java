b2b6cefbe0c9c55ff62344bba56ac2cb4da29495
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-07-14 12:30:29 +0530

    Time: 2 ms (99.54%), Space: 47.2 MB (16.01%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.java
@@ -4,16 +4,16 @@ class Solution {
         int m=grid[0].length;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                // check for primary
-                if(i==j){
+                
+                if(i==j){ // check for primary diagonal
                     if(grid[i][j]==0){
                         return false;
                     }
-                }else if(i+j==n-1 && i!=j){
+                }else if(i+j==n-1 && i!=j){ //check for secondary diagonal
                     if(grid[i][j]==0){
                         return false;
                     }
-                }else{
+                }else{ //check for non-diagonal el
                     if(grid[i][j]!=0){
                         return false;
                     }
