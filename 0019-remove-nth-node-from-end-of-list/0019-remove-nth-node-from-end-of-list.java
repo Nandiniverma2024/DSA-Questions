d8424fc9a3baa8eb9b437da2d4ef9fd5f6b90bf7
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-07-17 18:10:27 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.19%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,35 +10,44 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // Edge Case
+        // Edge Case11
         if(head==null || head.next==null){
             return null;
         }
-        // nthNode => size-n+1 (from begining for 0 based indexing)
-        int prevNthNode=sizeLL(head)-n;
+        ListNode slow=head;
+        ListNode fast=head;
 
-        if(n==sizeLL(head)){
-            head=head.next;
-        }
-        
+        // Find size of LL
         ListNode temp=head;
-        int len=1;
-        while(temp!=null && temp.next!=null){
-            if(len==prevNthNode){
-                temp.next=temp.next.next;
-            }
-            len++;
+        int size=0;
+        while(temp!=null){
+            size++;
             temp=temp.next;
         }
+        // Edge case2 (agr size n ke equal h to delete head)
+        if(size==n){
+            head=head.next;
+            return head;
+        }
+
+        // Step1: move fast n steps
+        while(n>0){
+            fast=fast.next;
+            n--;
+        }
+
+        // Step2: move slow and fast both 1 step
+        // isme jb fast last index pr hoga tb slow target ke just piche hoga
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        // Deleting target node
+        slow.next=slow.next.next;
+
         return head;
     }
-    public int sizeLL(ListNode head){
-        ListNode temp=head;
-        int count=0;
-        while(temp!=null){
-            count++;
-            temp=temp.next;
-        }
-        return count;
-    }
 }
+
+// Optimal approach
