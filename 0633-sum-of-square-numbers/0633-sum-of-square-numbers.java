8ca733e1d3207b8472af4cad4b6e8e42ab83b34e
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-07-28 22:06:46 +0530

    Time: 5 ms (19.34%), Space: 42.3 MB (14.91%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long start=0;
+        long end=(long)Math.sqrt(c);
+        while(start<=end){
+            if((start*start) + (end*end)==c){
+                return true;
+            }else if((start*start)+(end*end)>c){
+                end--;
+            }else if((start*start)+(end*end)<c){
+                start++;
+            }
+        }
+        return false;
+    }
+}
