3c86bb5f1dfaa42992c972fe22b341557d1f59d5
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-04 21:57:47 +0530

    Time: 4 ms (8.17%), Space: 46.3 MB (5.39%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n=arr.length;
+        int large=Integer.MIN_VALUE;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0; i<n; i++){
+            large=Math.max(large, arr[i]);
+            set.add(arr[i]);
+        }
+
+        
+
+
+        int val=0, cnt=0;
+        // Find number with in range
+        for(int i=1; i<=large; i++){
+            if(!set.contains(i)){
+                val=i;
+                cnt++;
+                if(cnt==k){
+                    return val;
+                }
+            }
+        }
+        return large + (k - cnt);
+    }
+}
