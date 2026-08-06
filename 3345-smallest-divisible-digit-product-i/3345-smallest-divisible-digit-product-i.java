d0213e5b1adaff0f91ec64ee806b29cec336f814
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-06 10:14:38 +0530

    Time: 1 ms (100%), Space: 42.6 MB (66.49%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -8,6 +8,8 @@ class Solution {
                 prod*=ld;
                 temp/=10;
             }
+            // outer while loop tab tak chalega jb tak return condition execute nhi ho jati 
+            // kuki is overall while(true) loop se bhar niklne ka return hi one and only tarika h
             if(prod%t==0){
                 return n;
             }
