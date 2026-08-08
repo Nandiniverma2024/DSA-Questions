4fa3fca17e793ec102a2d65e9a627479688a496b
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-08 13:09:42 +0530

    Time: 0 ms (100%), Space: 44.2 MB (87.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,24 +1,40 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> ans=new ArrayList<>();
-    
-        return solve(n, n, "", ans);
-    }
-    public List<String> solve(int openingBracket, int closingBracket, String str, List<String> ans){
-        // Base Case
-        if(openingBracket==0 && closingBracket==0){
-            ans.add(str);
-            return ans;
-        }
-        // choice 1 (add opening bracket)
-        if(openingBracket>0){
-            solve(openingBracket-1, closingBracket, str+"(", ans);
-        }
-        // Choice 2 (add closing brackets)
-        if(closingBracket>openingBracket){
-            solve(openingBracket, closingBracket-1, str+")", ans);
-        }
+        List<String> ans = new ArrayList<>();
+
+        StringBuilder str = new StringBuilder();
+
+        solve(n, n, str, ans);
 
         return ans;
+    }
+
+    public void solve(int open, int close, StringBuilder str, List<String> ans) {
+        // base case 
+        if(open == 0 && close == 0) {
+            ans.add(str.toString());
+            return;
+        }
+
+        // choice 1 --> add "("
+        if(open > 0) {
+            str.append('(');
+
+            solve(open-1, close, str, ans);
+
+            //backtracking
+            str.deleteCharAt(str.length() - 1);
+
+        }
+        // choice 2 --> add ")"
+        if(close > open) {
+            str.append(')');
+
+            solve(open, close-1, str, ans);
+
+            //backtracking
+            str.deleteCharAt(str.length() - 1);
+
+        }
     }
 }
