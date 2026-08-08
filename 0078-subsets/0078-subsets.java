f92a3efcfa3c09ee2f275f029e24a17027350a8e
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-08 13:47:26 +0530

    Time: 1 ms (86.61%), Space: 44.3 MB (20.53%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> li=new ArrayList<>();
+
+        solve(nums, 0, li, ans);
+
+        return ans;  //(since return type of helper func is void)
+    }
+    public void solve(int nums[], int i, List<Integer> li, List<List<Integer>> ans){
+        // base Case
+        if(i==nums.length){
+            ans.add(new ArrayList<>(li));
+            return;
+        }
+
+        // Choice 1 => Take el
+        li.add(nums[i]);
+        solve(nums, i+1, li, ans);
+        // Backtrack(remove last el)
+        li.remove(li.size()-1);
+
+        // Choice 2 => not take any el
+        solve(nums, i+1, li, ans);
+    }
+}
