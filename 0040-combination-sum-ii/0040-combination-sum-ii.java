8dc02cd97113353459e2aa9e69f4af80bd17ab89
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-09 12:33:53 +0530

    Time: 6 ms (72.8%), Space: 45.3 MB (53.58%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> li=new ArrayList<>();
+        Arrays.sort(candidates);
+        solve(candidates, target, 0, li, ans);
+
+        return ans;
+    }
+    public void solve(int arr[], int target, int idx, List<Integer> li, List<List<Integer>> ans){
+        // Base Case
+        if(target==0){
+            ans.add(new ArrayList<>(li));
+            return;
+        }
+
+        for(int i=idx; i<arr.length; i++){
+            if(i>idx && arr[i]==arr[i-1]){
+                continue;
+            }
+            if(arr[i]>target){
+                break;
+            }
+            li.add(arr[i]);
+            solve(arr, target-arr[i], i+1, li, ans);
+            li.remove(li.size()-1);
+        }
+    }
+}
