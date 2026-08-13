a14b1d12f3086515a187d6cb921ee869236b7a75
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-13 23:34:59 +0530

    Time: 13 ms (72.06%), Space: 59.5 MB (53.09%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,8 +1,7 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n=nums.length;
-        // make it binary
-        for(int i=0; i<n; i++){
+        // Make it Binary
+        for(int i=0; i<nums.length; i++){
             if(nums[i]%2==0){
                 nums[i]=0;
             }else{
@@ -10,23 +9,37 @@ class Solution {
             }
         }
 
-        return atMost(nums, k) - atMost(nums, k-1);
+        return atMost(nums, k) - atMost(nums, k-1); 
     }
-    public int atMost(int nums[], int k){
-        int left=0, right=0; 
-        int n=nums.length;
-        int sum=0;
-        int count=0;
-        while(right<n){
-            // do calculations
-            sum+=nums[right];
-            while(sum>k){
-                sum-=nums[left];
-                left++;
-            }
-            count+=right-left+1;
-            right++;
+    public int atMost(int[] nums, int k) {
+        int left=0, right=0;
+        int n=nums.length, sum=0;
+        int len=0;
+
+        // Lekin agar:
+        // k = 0
+        // toh formula banega:
+        // atMost(nums, 0) - atMost(nums, -1)
+
+        if(k<0){
+            return 0;
         }
-        return count;
+
+        while(right<n){
+            sum+=nums[right];
+            // jitni len , utne us index pr present subarrays
+            if(sum <= k){ //no of valid subarrays
+                len+=right-left+1;
+                right++;
+            }else if(sum>k){
+                while(sum>k){
+                    sum-=nums[left];
+                    left++;
+                }
+                len+=right-left+1;
+                right++;
+            }
+        }
+        return len;
     }
 }
