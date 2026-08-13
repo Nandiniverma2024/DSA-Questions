0849a030121d094fb1781c3a87485aaae4f6d48c
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-13 22:28:15 +0530

    Time: 1 ms (99.96%), Space: 50.9 MB (65.94%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,25 +1,35 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int cnt=0;
-        HashMap<Integer, Integer> map=new HashMap<>();
-        map.put(0,1);
-
-        // map => (Prefix sum,freq)
-
-        int n=nums.length;
-        int sum=0, prefixSum=0;
-
-
-        for(int i=0; i<n; i++){
-            sum+=nums[i];
-            prefixSum=sum-goal;
-            if(map.containsKey(prefixSum)){
-                cnt+=map.get(prefixSum);
-            }
-            
-            map.put(sum, map.getOrDefault(sum, 0)+1);
-            
+        return atMost(nums, goal) - atMost(nums, goal-1);
+        
+    }
+    public int atMost(int[] nums, int goal) {
+        if(goal<0){
+            return 0;
         }
-        return cnt;
+        int left=0, right=0, n=nums.length;
+        int sum=0;
+        int len=0;
+
+        while(right<n){
+            sum+=nums[right];
+            if(sum<=goal){
+                // jitni length h utne hi subarray h
+                // indx=0, len=1, subarrya=1
+                // indx=1, len=2, subarrya=2
+                // indx=2, len=3, subarrya=3
+                len+=right-left+1;
+                right++;
+            }else if(sum > goal){
+                // This while loop changing sum in every iteration, prefix is also changing in every iteration so i must need to use expression
+                while(sum > goal){
+                    sum-=nums[left];
+                    left++;
+                }
+                len+=right-left+1;
+                right++;
+            }
+        }
+        return len;
     }
 }
