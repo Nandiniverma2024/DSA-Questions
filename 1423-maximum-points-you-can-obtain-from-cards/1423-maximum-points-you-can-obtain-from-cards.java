421273b6592d266f9a122fa6bd082aac9cfec65e
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-14 11:55:38 +0530

    Time: 2 ms (99.74%), Space: 61.6 MB (73.85%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,34 +1,41 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n=cardPoints.length;
-        int left=0, right=0;
-        int sum=0, minSum=Integer.MAX_VALUE;
         int totalSum=0;
-        for(int i=0; i<cardPoints.length; i++){
+
+        // Find totalSum
+        for(int i=0; i<n; i++){
             totalSum+=cardPoints[i];
         }
 
+        // agr total cards hi k h , to vo hi to maximum honge
         if(n==k){
             return totalSum;
         }
+
+
+        // sliding window
+        // (maxSum k cards left ya right se niklne ke lia , find array ke mid m present minimum cards)
+        int left=0, right=0;
+        int sum=0, minSum=Integer.MAX_VALUE;
+
         while(right<n){
+            // globally sum cal krne se => hr iteration m add hoga
             sum+=cardPoints[right];
             if(right-left+1 < n-k){
                 right++;
             }else if(right-left+1 == n-k){
                 minSum=Math.min(minSum, sum);
+                // Move the fixed size window
                 sum-=cardPoints[left];
                 left++;
                 right++;
             }
         }
-        
+        // maxSum=totalSum-minSum;
         return totalSum-minSum;
     }
 }
 
 
-// find window size of n-k , such it have minimum value(minimum el => kuki mid wale el h , or inki window hogi hi hogi)
-
-// totaklcards i.e n (k cards window nhi bna rhe)
-// totalcards-k , i.e => (n-k)
+// Goal => find max k Cards either from left or from right
