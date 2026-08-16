8fc028e721c2c0c68c1d981f626883506371b440
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-16 11:13:23 +0530

    Time: 16 ms (56.03%), Space: 47.1 MB (49.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,55 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-        // Edge case
+        // edge Case
         if(t.length()>s.length()){
             return "";
         }
-        
-        // store all characters of t in map
-        HashMap<Character, Integer> map=new HashMap<>();
+
+        HashMap<Character, Integer> tMap=new HashMap<>();
         for(int i=0; i<t.length(); i++){
             char ch=t.charAt(i);
-            map.put(ch, map.getOrDefault(ch,0)+1);
+            tMap.put(ch, tMap.getOrDefault(ch,0)+1);
         }
+        
+        int cnt=tMap.size();
 
-        int count=map.size();
-
-        // start traverseing string s, and finding min window
-        int left=0, right=0, start=0;
-        int minLen=Integer.MAX_VALUE;
-        int n=s.length();
+        // sliding window on s
+        int left=0, right=0, n=s.length();  
+        int start=0, minLen=Integer.MAX_VALUE;
 
         while(right<n){
-            // Do calculation
             char ch=s.charAt(right);
-            if(map.containsKey(ch)){
-                map.put(ch, map.get(ch)-1); //reduce freq of that el
- 
-                if(map.get(ch)==0){
-                    count--;
+            if(tMap.containsKey(ch)){
+                tMap.put(ch, tMap.get(ch)-1); //reduce freq
+                if(tMap.get(ch)==0){
+                    cnt--;
                 }
             }
 
-        
-            while(count==0){
-                // find ans from calculation
-                if(right-left+1 < minLen){
+            while(cnt==0){
+                if(minLen > right-left+1){
                     minLen=right-left+1;
                     start=left;
                 }
-                // Taki next window ko check krne ke lia characters ki freq mil jay jinse next window ke el comp honge
-                char leftChar=s.charAt(left);
-                if(map.containsKey(leftChar)){
-                    if(map.get(leftChar)==0){
-                        count++;
+                char leftCh=s.charAt(left);
+                if(tMap.containsKey(leftCh)){
+                    tMap.put(leftCh, tMap.get(leftCh)+1);
+                    if(tMap.get(leftCh)>0){
+                        cnt++;
                     }
-                    map.put(leftChar, map.get(leftChar)+1);
-
                 }
-                left++;
+                left++; 
+                
             }
             right++;
-        }
-        return minLen==Integer.MAX_VALUE?"":s.substring(start, start+minLen);
+        } 
+
+        // muje "s" string m "t" ke char mile hi nhi
+        if(minLen==Integer.MAX_VALUE){
+            return "";
+        } 
+
+        return s.substring(start, start+minLen);
     }
 }
