d557ea928d08e67c3095569d8152ff8cca3e7e3f
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-19 14:52:27 +0530

    Time: 2 ms (96.01%), Space: 45.8 MB (51.72%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,28 +2,31 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> li=new ArrayList<>();
-
+    
         solve(candidates, target, 0, li, ans);
 
         return ans;
+
     }
-    public void solve(int candidates[], int target, int i, List<Integer> li, List<List<Integer>> ans){
+    public void solve(int[] candidates, int target, int i, List<Integer> li, List<List<Integer>> ans){
         // Base Case
         if(i==candidates.length){
-            if(target==0){ //agr target km hote hote 0 ho jata h
+            if(target==0){
                 ans.add(new ArrayList<>(li));
             }
             return;
         }
 
-        // Choice => choose el
-        if(candidates[i]<=target){
-            li.add(candidates[i]);
-            solve(candidates, target-candidates[i], i, li, ans);
-            li.remove(li.size()-1);
+        if(target<0){
+            return;
         }
 
-        // choice => not choose
+        // Taken
+        li.add(candidates[i]);
+        solve(candidates, target-candidates[i], i, li, ans);
+        li.remove(li.size()-1);
+
+        // Not Taken
         solve(candidates, target, i+1, li, ans);
     }
 }
