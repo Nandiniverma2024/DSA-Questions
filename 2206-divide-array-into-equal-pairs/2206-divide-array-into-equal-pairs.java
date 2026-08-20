e22bba6688fc086325ac1cc849bbfde9e15a0f51
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-20 14:21:28 +0530

    Time: 8 ms (49.76%), Space: 46.4 MB (54.4%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -5,6 +5,7 @@ class Solution {
         for(int num:nums){
             map.put(num, map.getOrDefault(num,0)+1);
         }
+        // odd freq h to pair nhi hoga , return false(pair sirf even freq se bn sakta h)
         for(int key:map.keySet()){
             int freq=map.get(key);
             if(freq%2!=0){
