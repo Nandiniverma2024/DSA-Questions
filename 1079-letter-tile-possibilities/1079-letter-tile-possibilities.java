016de1043400ec53ba74d1af3e67b15ef2d57da7
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-22 23:12:55 +0530

    Time: 2 ms (88.75%), Space: 42.5 MB (95.82%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,33 +1,26 @@
 class Solution {
     public int numTilePossibilities(String tiles) {
-        // StringBuilder sb=new StringBuilder();
-        ArrayList<String> li=new ArrayList<>();
-        HashSet<String> set=new HashSet<>();
-
-        solve(tiles, "", li);
-        for(int i=0; i<li.size(); i++){
-            set.add(li.get(i));
+        int freq[]=new int[26];
+        for(int i=0; i<tiles.length(); i++){
+            char ch=tiles.charAt(i);
+            freq[ch-'A']++;
         }
-        return set.size()-1; //to remove empty string from starting
+
+        return solve(freq);
     }
+    public int solve(int freq[]){
+        int count=0;
+        for(int i=0; i<26; i++){
+            if(freq[i]==0){
+                continue;
+            }
+            count++;
 
-    public void solve(String s, String ans, ArrayList<String> li){
-        // s.length()==0 i.e hmne sare combination try kr lia
-        if(s.length()==0){
-            li.add(ans); //initially ans empty h
-            return;
+            freq[i]--;
+            count+=solve(freq);
+            // backtrack
+            freq[i]++;
         }
-        li.add(ans);
-        for(int i=0; i<s.length(); i++){
-            char ch=s.charAt(i);
-            // s="abcde" // c wala choose kr lia , want to skip this in next iteration 
-            // remove char at each level
-            String newStr=s.substring(0,i) + s.substring(i+1, s.length());
-            solve(newStr, ans+ch, li); 
-            // ans+ch =>
-            //add one char at each level ("a" at first level)
-            //add next char at each level ("b" at first level)
-            //add next char at each level ("c" at first level)
-        }
+        return count;
     }
 }
