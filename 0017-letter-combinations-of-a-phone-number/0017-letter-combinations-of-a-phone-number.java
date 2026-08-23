8bc3a6a3eb6540959b1bfe1fb466d784f5057f05
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-23 23:19:11 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,26 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        // Base Case
-        if(digits.length()==0){
-            return new ArrayList<>();
-        }
-        String arr[]={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
         List<String> ans=new ArrayList<>();
+        String comb[]={"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         StringBuilder sb=new StringBuilder();
-
-        // idx=0, for traversing on digits
-        solve(digits, 0, sb, arr, ans);
+        solve(digits, 0, sb, comb, ans);
 
         return ans;
     }
-    public void solve(String digits, int idx, StringBuilder sb, String arr[], List<String> ans){
+    public void solve(String digits, int idx, StringBuilder sb, String comb[], List<String> ans){
         // Base Case
         if(idx==digits.length()){
             ans.add(sb.toString());
             return;
         }
+        char ch=digits.charAt(idx);
+        int res=Character.getNumericValue(ch); //since index are always in numerical value
+        String temp=comb[res];
 
-        char ch=digits.charAt(idx); // '2'
-        int result = Character.getNumericValue(ch); // convert '2' => 2
-
-
-        String temp=arr[result];  //arr[2] => "abc"
-
-        // Traversing on string "abc"
         for(int i=0; i<temp.length(); i++){
-            sb.append(temp.charAt(i)); //"a"
-            solve(digits, idx+1, sb, arr, ans); // idx+1 => 3 => "def"
+            sb.append(temp.charAt(i));
+            solve(digits, idx+1, sb, comb, ans);
             sb.deleteCharAt(sb.length()-1);
-            // ake sath d aane ke bad , d removeb  ho jayga (by deleteCharAt)
         }
     }
 }
