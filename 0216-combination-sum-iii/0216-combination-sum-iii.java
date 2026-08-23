1a80d66c2a8c80be6ccd54bde619c32d6f2e8f66
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-23 22:20:53 +0530

    Time: 0 ms (100%), Space: 42.4 MB (47.88%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -2,37 +2,27 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> li=new ArrayList<>();
-        int candidates[]={1,2,3,4,5,6,7,8,9};
-
-        solve(candidates, k, n, 0, li, ans);
+        int nums[]={1,2,3,4,5,6,7,8,9};
+        solve(nums, k, n, 0, 0, li, ans);
 
         return ans;
-
-    } 
-    public void solve(int arr[], int k, int target, int idx, List<Integer> li,List<List<Integer>> ans){
-        // Base Case
-        if(k==0 && target==0){
-            // target=0, i.e  ouput list m ek combination aa chuka 
-            ans.add(new ArrayList<>(li));
+    }
+    public void solve(int nums[], int k, int n, int i, int sum, List<Integer> li, List<List<Integer>> ans){
+        if(i==nums.length){
+            if(sum==n && li.size()==k){
+                ans.add(new ArrayList<>(li));
+            }
             return;
         }
 
-        for(int i=idx; i<arr.length; i++){
-            if(arr[i]>target){
-                break;
-            }
-           
-      
-            li.add(arr[i]); //choice made
-            solve(arr, k-1, target-arr[i], i+1, li, ans); //choice exploration
-            li.remove(li.size()-1); //backtracking
-        }
+        // Taken
+        li.add(nums[i]);
+        sum+=nums[i];
+        solve(nums, k, n, i+1, sum, li, ans);
+        sum-=nums[i];
+        li.remove(li.size()-1); //backtrack
+
+        // Not Taken
+        solve(nums, k, n, i+1, sum, li, ans);
     }
 }
-
-// k=3, n=7
-
-// _  _  _
-
-//  |
-//  1-9
