052a82ed861c65f48642980ff09001c0905f0d07
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-08-26 23:08:42 +0530

    Time: 2 ms (59.04%), Space: 42.5 MB (12.35%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int totalNQueens(int n) {
+        List<List<String>> ans=new ArrayList<>();
+        // List<String> li=new ArrayList<>();
+
+        char board[][]=new char[n][n];
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                board[i][j]='.';
+            }
+        }
+
+        solve(board, 0,  n, ans);
+
+        return ans.size();
+    }
+    public void solve(char board[][], int row, int n, List<List<String>> ans){
+        // Base Case
+        if(row==n){
+            List<String> li=new ArrayList<>();
+            for(int i=0; i<n; i++){ //for row
+                li.add(new String(board[i]));//convert character array into string
+            }
+            ans.add(new ArrayList<>(li));
+        }
+        for(int j=0; j<n; j++){
+            if(isSafe(board, row, j, n)){
+                board[row][j]='Q'; //Q batha di
+                solve(board, row+1, n, ans); // call for next row
+                board[row][j]='.'; //backtrack
+            }
+        }
+    }
+    public boolean isSafe(char board[][], int row, int col, int n){
+        // Vertically up
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // Left diagonal
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        // Right diagonal
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
