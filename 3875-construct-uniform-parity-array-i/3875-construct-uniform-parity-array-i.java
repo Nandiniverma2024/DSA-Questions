9b20c8825298573431bcb99859994039bfae8998
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-09-02 23:55:59 +0530

    Time: 0 ms (100%), Space: 44.9 MB (87.6%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,35 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n=nums1.length;
-        boolean flag=false;
-
-        // agr sare even el h
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2==0){
-                return true;
-            }
-        }
-        // agr odd el h
-        for(int i=0; i<n; i++){
-            if(nums1[i]%2!=0){
-                return true;
-            }
-        }
-
-        if(nums1[n-1]%2 == 0){ //even
-            for(int i=0; i<n-1; i++){
-                if( (nums1[i]%2!=0 || (nums1[i]-nums1[i+1]) %2 ==0 ) && (nums1[i]%2==0 || (nums1[i]-nums1[i+1]) %2 !=0)){
-                    return true;
-                }
-            }
-        }else if(nums1[n-1]%2 != 0){ //odd
-             for(int i=0; i<n-1; i++){
-                if((nums1[i]%2==0 || (nums1[i]-nums1[i+1]) %2 !=0 ) && (nums1[i]%2!=0 || (nums1[i]-nums1[i+1]) %2 ==0)){
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return true;
     }
 }
