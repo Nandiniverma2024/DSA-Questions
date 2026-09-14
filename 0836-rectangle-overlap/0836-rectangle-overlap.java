1f7f3b99743653928e3ccec9f634f65f0e280058
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-09-14 05:45:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (17.64%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.java
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // Non-overlap condition
         if (rec1[2] <= rec2[0] || 
             rec2[2] <= rec1[0] || 
             rec1[3] <= rec2[1] || 
