321cb90d85fa33608ef55ec58ed44902d099e50c
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-09-15 20:33:23 +0530

    Time: 1 ms (29.32%), Space: 42.3 MB (81.12%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -2,9 +2,17 @@ class Solution {
     public int hammingWeight(int n) {
         int res=0; 
         for(int i=0; i<32; i++){
-            res += n&1;
-            n=n>>1;
+            res += n&1; //for tracking count
+            n=n>>1; //(lsb i.e last bit n ke right shit se decide hogi)
         }
         return res;
     }
 }
+
+// eg => n=13 , i.e 1101 
+// 1 -> n&1 => 1101 & 0001 => cnt=1 then n>>1  =>  101 
+// 2 -> n&1 =>  101 & 0001 => cnt=2 then n>>1  =>  10 
+// 3 -> n&1 =>   10 & 0001 => cnt=2 then n>>1  =>   1 
+// 4 -> n&1 =>    1 & 0001 => cnt=3 then n>>1  =>   0 
+
+
