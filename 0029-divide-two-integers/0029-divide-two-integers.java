ea28087da07ca52441e1560ac36a28abe8bb9406
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-09-19 14:23:55 +0530

    Time: 1 ms (98.52%), Space: 42.8 MB (14.63%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,32 +1,36 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if(dividend==divisor){
-            return 1;
+        if(divisor == dividend){
+            return 1; //1 hi bar jayga to quotient = 1
         }
+
         boolean sign=true;
-        if(dividend <=0 && divisor>0){
+
+        // check for sign
+        if(dividend <= 0 && divisor>0){
             sign=false;
-        }else if(dividend >=0 && divisor<0){
+        }else if(divisor<0 && dividend >= 0){
             sign=false;
         }
 
         long n=Math.abs((long) dividend);
         long d=Math.abs((long) divisor);
+
         long ans=0;
 
         while(n>=d){
             int cnt=0;
-            while(n>= (d<<(cnt+1))){
+            while(n>= (d<< (cnt+1))){
                 cnt++;
             }
-            ans += (1L<<cnt);
             n -= d<<cnt;
+            ans += 1L<<cnt;
         }
 
-        if(ans > Integer.MAX_VALUE && sign==true){
+        if(ans>Integer.MAX_VALUE && sign==true){
             return Integer.MAX_VALUE;
         }
-        if(ans > Integer.MAX_VALUE && sign==false){
+        if(ans>Integer.MAX_VALUE && sign==false){
             return Integer.MIN_VALUE;
         }
 
