af19846d51f630c23c8fd290aec3f3ea4f25790b
Nandini Verma <146837622+Nandiniverma2024@users.noreply.github.com>
2026-09-22 21:05:12 +0530

    Time: 1 ms (83.95%), Space: 42.7 MB (73.02%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,25 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character, String> map=new HashMap<>();
-        String word[]=s.split(" ");
+        HashMap<Character, String> map1 = new HashMap<>();
+        HashMap<String, Character> map2 = new HashMap<>();
 
-        if(pattern.length()!=word.length){
-            return false; //since we need one word for each character
+        String[] words = s.split(" ");
+
+        if(pattern.length() != words.length) {
+            return false;
         }
 
-        for(int i=0; i<pattern.length(); i++){
-            char ch=pattern.charAt(i);
-            if(!map.containsKey(ch)){
-                // agr pheli bar koi char aaya, or jo word uske sath map krna hagr vo phele se present h map m return false
-                if(map.containsValue(word[i])){
-                    return false;
-                }
-                map.put(ch, word[i]); //first time char aaya h, map it with word
-            }else{ //agr char already map m h
-                if(!map.get(ch).equals(word[i])){ //ch pr phel jo mapped word tha, agr vo current word ke equal nhi h to 
-                    return false;
-                }
+        for(int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+
+            if(map1.containsKey(ch) && !map1.get(ch).equals(words[i])) {
+                return false;
             }
+
+            if(map2.containsKey(words[i]) && map2.get(words[i]) != ch) {
+                return false;
+            }
+
+            map1.put(ch, words[i]);
+            map2.put(words[i], ch);
         }
         return true;
     }
